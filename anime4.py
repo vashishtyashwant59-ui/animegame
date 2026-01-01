1c4282b2a5d04278c989b47062c1b4e2b5fae265
vashishtyashwant59-ui <vashishtyashwant59@gmail.com>
2026-01-01 16:14:59 +0530

    Update anime4.py

--- anime4.py
+++ anime4.py
@@ -80,7 +80,7 @@ ROLES = ["Captain", "Vice Captain", "Tank", "Healer", "Assassin", "Support 1", "
 HEALER_BONUS = 2.3
 
 # Forfeit settings (seconds)
-FORFEIT_TIMEOUT = 5 * 1  # 5 minutes
+FORFEIT_TIMEOUT = 5 * 60  # 5 minutes
 FORFEIT_CHECK_INTERVAL = 50  # check every 30s
 
 # --- INITIALIZATION ---
